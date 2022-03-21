6783aaadcdafb29ca6866c7c7241f611dcf4b2ab
kalynovavs <valentinka18.12.95@gmail.com>
2022-03-21 17:49:19 +0300

    add test for over a lot of money

--- src/test/java/ru/netology/pageobject/MoneyTransferTest.java
+++ src/test/java/ru/netology/pageobject/MoneyTransferTest.java
@@ -88,4 +88,30 @@ public class MoneyTransferTest {
         Assert.assertEquals(expectedToBalance,actualToBalance);
         Assert.assertEquals(expectedFromBalance,actualFromBalance);
     }
+
+    @Test
+    void shouldTransferALotOfMoney() {
+        //auth
+        open("http://localhost:9999");
+        var amount = 1000000;
+        var firstCard = DataHelper.getFirstCardInfo();
+        var secondCard = DataHelper.getSecondCardInfo();
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validCode(verificationCode);
+        // make transfer
+        var balanceStartFrom = dashboardPage.getCardBalanceFor(secondCard.getId());
+        var balanceStartTo = dashboardPage.getCardBalanceFor(firstCard.getId());
+        var transferPage = dashboardPage.makeTransfer(firstCard);
+        var newDashboardPage = transferPage.transferMoney(amount, secondCard);
+        // check result
+        int expectedToBalance = balanceStartTo + amount;
+        int actualToBalance = newDashboardPage.getCardBalanceFor(firstCard.getId());
+        int expectedFromBalance = balanceStartFrom - amount;
+        int actualFromBalance = newDashboardPage.getCardBalanceFor(secondCard.getId());
+        Assert.assertEquals(expectedToBalance,actualToBalance);
+        Assert.assertEquals(expectedFromBalance,actualFromBalance);
+    }
 }
